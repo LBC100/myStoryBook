1e4e2fb264f052f4f8dec9a83eb9703134a30826
LBC <847129673@qq.com>
2018-11-20 13:38:13 +0800

    安卓端启动页全屏

--- android/app/src/main/java/com/mystorybook/MainActivity.java
+++ android/app/src/main/java/com/mystorybook/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle saveInstanceState){
-        SplashScreen.show(this);
+        SplashScreen.show(this, R.style.SplashScreenTheme);
         super.onCreate(saveInstanceState);
     }
 }
--- app/Root.js
+++ app/Root.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import SplashScreen from "react-native-splash-screen";
+import {
+    StyleSheet,
+    BackHandler,
+    View,
+    ToastAndroid,
+    AsyncStorage,
+    Dimensions,
+    ActivityIndicator,
+    StatusBar,
+    Platform
+} from 'react-native';
 
 import AppNavigator from './config/AppNavigator';
-import SplashScreen from "react-native-splash-screen";
+
 
 class Root extends Component {
     componentDidMount() {
+        StatusBar.setBarStyle('light-content');
+        Platform.OS == 'android' ? StatusBar.setBackgroundColor('rgba(66,175,240,.0)') : '';
+        Platform.OS == 'android' ? StatusBar.setTranslucent(true) : '';
         SplashScreen.hide(); // 隐藏启动屏
     }
     render() { 
